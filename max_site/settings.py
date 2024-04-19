6d01ef5cb7b2aaee7d8d908cbf09f7cada33ec69
freazzz-ya <Freazzzing36@yandex.ru>
2024-04-19 20:31:09 +0300

    docker

--- max_site/settings.py
+++ max_site/settings.py
@@ -74,10 +74,9 @@ WSGI_APPLICATION = 'max_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
-}
-
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
